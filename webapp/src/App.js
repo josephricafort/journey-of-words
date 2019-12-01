370488b0e3ac155cfdb3e31254d1d24c4606917b
josephricafort <josephricafortjr@gmail.com>
2019-12-01 21:54:15 +0800

    Add basic react-leaflet

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Map } from "react-leaflet";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        style={{ height: "480px", width: "100%" }}
+        zoom={1}
+        center={[-0.09, 51.505]}
+      ></Map>
     </div>
   );
 }
---
+++ webapp/src/container/Map/Map.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./Map.module.css";
+
+const Map = () => {
+  return <div className={styles.mapid}></div>;
+};
+
+export default Map;
